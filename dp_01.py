9efe338e4320b438259eaf93fba9cefc68376cca
PercyUwUOwO <96863206+PercyUwUOwO@users.noreply.github.com>
2021-12-30 16:05:38 +0700

    Add files via upload

---
+++ dp_01.py
@@ -0,0 +1,19 @@
+def first_lyrics():
+    print("Go, team go!")
+    print("We can do it.")
+def second_lyrics():
+    print("We're the 'cats,")
+    print("We'll leave it at that.")
+def print_fight_song():
+    first_lyrics()
+    print("")
+    first_lyrics()
+    second_lyrics()
+    first_lyrics()
+    print("")
+    first_lyrics()
+    second_lyrics()
+    first_lyrics()
+    print("")
+    first_lyrics()
+    
